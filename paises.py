738ddc20cefb3ce47c74255a18800539ea492ccd
LuisOsorio <losoriossaenz@gmail.com>
2026-03-06 19:39:34 -0500

    se agrega un while para que el programa no termine hasta que el usuario lo desee

--- paises.py
+++ paises.py
@@ -18,20 +18,20 @@ def obtener_paises():
         print("Error al obtener los datos: ", e)
         return []
 
-def seleccion_de_busqueda(PAIS, BUSQUEDA, tipo_busqueda):
+def seleccion_de_busqueda(PAISES, BUSQUEDA, tipo_busqueda):
     if tipo_busqueda == 1: #busqueda lineal
-        for i in PAIS:
+        for i in PAISES:
             if i['nombre'] == BUSQUEDA:
                 return i['googleMaps']
         return "nada....."
     elif tipo_busqueda == 2: #busqueda binaria
         izquierda = 0
-        derecha = len(PAIS) - 1
+        derecha = len(PAISES) - 1
         while izquierda <= derecha:
             medio = (izquierda + derecha) // 2
-            valor_medio = PAIS[medio]['nombre']
+            valor_medio = PAISES[medio]['nombre']
             if valor_medio == BUSQUEDA:
-                return PAIS[medio]['googleMaps']
+                return PAISES[medio]['googleMaps']
             elif valor_medio < BUSQUEDA:
                 izquierda = medio + 1
             else:
@@ -41,7 +41,13 @@ def seleccion_de_busqueda(PAIS, BUSQUEDA, tipo_busqueda):
     else: 
         print("seleccione la opcion correcta")    
         
-paises = obtener_paises()
-name_country = input("Ingrese el nombre del pais en ingles: ")
-Selection = int(input("Seleccione el tipo de busqueda: \n1.Lineal \n2.Binaria \n: "))
-print("Nombre del pais", name_country ,"con siguiente link: ",seleccion_de_busqueda(paises, name_country, Selection))
+Exit = False
+while Exit == False:
+    paises = obtener_paises()
+    name_country = input("Ingrese el nombre del pais en ingles: ")
+    Selection = int(input("Seleccione el tipo de busqueda: \n1.Lineal \n2.Binaria \n: "))
+    print("Nombre del pais", name_country ,"con siguiente link: ",seleccion_de_busqueda(paises, name_country, Selection))
+    padlock = input("Desea salir? (s/n): ").lower()
+    if padlock == "s":
+        Exit = True
+        print("Saliendo, ADIOS!")
